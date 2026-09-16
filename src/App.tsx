a34e43d0879c5bbd0332676f8c53812550a07ce4
Ramees R <rameesr4s@gmail.com>
2026-09-17 00:21:13 +0530

    Add authentication route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -12,7 +14,8 @@ const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/auth" element={<Auth />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
